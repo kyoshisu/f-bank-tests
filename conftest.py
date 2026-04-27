9a44228205eac044c088cdb80fb0c07a69ead755
unknown <brodagin11@gmail.com>
2026-04-27 23:09:14 +0500

    Fix ChromeDriver setup for GitHub Actions

--- conftest.py
+++ conftest.py
@@ -1,8 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture
 def driver():
@@ -12,8 +10,8 @@ def driver():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
     
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     
     yield driver
+    
     driver.quit()
