8c5357b10ab5c9e34dd93a586b37f4908297ad5d
unknown <brodagin11@gmail.com>
2026-04-27 23:11:27 +0500

    Fix

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import os
 
 @pytest.fixture
 def driver():
@@ -9,8 +11,17 @@ def driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-gpu")
     
-    driver = webdriver.Chrome(options=chrome_options)
+    # Путь к chromium-browser и chromedriver в Ubuntu
+    if os.environ.get('CI'):
+        chrome_options.binary_location = "/usr/bin/chromium-browser"
+        service = Service(executable_path="/usr/bin/chromedriver")
+    else:
+        from webdriver_manager.chrome import ChromeDriverManager
+        service = Service(ChromeDriverManager().install())
+    
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     
     yield driver
     
